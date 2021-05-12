557e49743a8aa7d520df93fd6d9c120f62454877
Emmanuel Lagrée <lagreeemmanuel@vivaldi.net>
2021-05-12 10:56:48 +0200

    Last commit

--- main_perceptual_decision_alphanumeric_class.py
+++ main_perceptual_decision_alphanumeric_class.py
@@ -45,7 +45,7 @@ block_1 = design.Block(name = "Partial report")
 
 choice_nb_digits_p = [] # For each trial, remember whether there are a single or three digits presented
 all_lists_of_keyboard_constants_p = [] # For each trial, remember the list of keyboard constants used
-list_of_lists_allowed_digits_p = [] # When we check validity, we need to compare responses with 'allowed' digits
+all_lists_of_allowed_digits_p = [] # When we check validity, we need to compare responses with 'allowed' digits
 
 for k in range(N_TRIALS): # We design the N_TRIALS = 4 trials for partial report
 	nb_digits, list_of_characters_used = usf.make_list(set_chosen_for_exp)
@@ -57,7 +57,7 @@ for k in range(N_TRIALS): # We design the N_TRIALS = 4 trials for partial report
 	all_lists_of_keyboard_constants_p.append(list_of_keyboardconstants_used)
 	
 	list_of_allowed_digits = usf.remove_letters_from_list_of_char(list_of_characters_used)
-	list_of_lists_allowed_digits_p.append(list_of_allowed_digits)
+	all_lists_of_allowed_digits_p.append(list_of_allowed_digits)
 	
 	block_1.add_trial(single_trial)
 
@@ -66,7 +66,7 @@ block_2 = design.Block(name = "Whole report")
 
 choice_nb_digits_w = [] # Same as for partial report
 all_lists_of_keyboard_constants_w = [] # Same as for partial report
-list_of_lists_char_w = []
+all_lists_of_char_w = []
 
 for j in range(N_TRIALS): # We design the trials for whole report
 	nb_digits, list_of_characters_used = usf.make_list(set_chosen_for_exp)
@@ -78,7 +78,7 @@ for j in range(N_TRIALS): # We design the trials for whole report
 	list_of_keyboardconstants_used = usf.characters2keyboard_constants(list_of_characters_used, dict_char_to_keyboardconstants)
 	all_lists_of_keyboard_constants_w.append(list_of_keyboardconstants_used)
 	
-	list_of_lists_char_w.append(list_of_characters_used)
+	all_lists_of_char_w.append(list_of_characters_used)
 	
 	
 exp.add_block(block_1)
@@ -97,11 +97,11 @@ exp.add_data_variable_names(['n_set', ' is_partial', ' is_3_digits',' #trial', '
 control.start(skip_ready_screen = True)
 
 
-def run_session(exp, block, is_partial, list_exposure_duration, all_lists_of_keyboard_constants, list_of_lists_validity, choice_nb_digits, instructions, nb_set):
+def run_session(exp, block, is_partial, list_exposure_duration, all_lists_of_keyboard_constants, all_lists_validity, choice_nb_digits, instructions, nb_set):
 	""" This is the most important function of the project : it runs half the experiment : either the partial 
 	experiment or the whole experiment """
 	
-	""" The variable < list_of_lists_validity > is called to check the validity of the responses at each trial.
+	""" The variable < all_lists_validity > is called to check the validity of the responses at each trial.
 	For partial report : it is the list of the list of allowed digits at each trial
 	For whole report : it is the list of the list of the characters that are displayed at each trial """
 	
@@ -116,20 +116,22 @@ def run_session(exp, block, is_partial, list_exposure_duration, all_lists_of_key
 	careful_msg = stimuli.TextScreen("Careful :", f""" When reporting digits, do not use a numpad, but use the keys on top of your keyboard. Moreover, if you want to report a '1', press *only* '&'. Do not press Shift + '&', it is not necessary. Press SPACE BAR when ready""")
 	careful_msg.present()
 	exp.keyboard.wait(SPACE_BAR)
-	blankscreen.present()
-	
-	circle.present()
-	exp.clock.wait(100)
+	blankscreen.present()	
 	
 	i = 0
 	exposure_duration = list_exposure_duration[i]
-	for trial in block.trials:
+	for trial in block.trials: 
+		# Run one trial and ask for responses, checks them and store
+		circle.present()
+		exp.clock.wait(200)
+		
 		for stimulus in trial.stimuli:
 			stimulus.present(clear = False)
 		exp.clock.wait(exposure_duration) 
 		blankscreen.present()
+		
 		response_to_one_trial, rt_trial = usf.get_data_of_single_trial(exp, is_partial, trial, 		all_lists_of_keyboard_constants[i], choice_nb_digits[i], i+1, nb_set)
-		validity_of_one_trial = usf.check_validity_of_response(is_partial, response_to_one_trial, list_of_lists_validity[i], choice_nb_digits[i])
+		validity_of_one_trial = usf.check_validity_of_response(is_partial, response_to_one_trial, all_lists_validity[i], choice_nb_digits[i])
 	
 		exp.data.add([nb_set, is_partial, choice_nb_digits[i] == 3, i+1, response_to_one_trial, validity_of_one_trial, rt_trial])
 		usf.display_transition_intertrials(blankscreen, list_transitions[i], exp)
@@ -141,12 +143,12 @@ order = usf.begin_with_partial_report()
 exposure_conditions = [90, 120, 150, 500]
 
 if order is True:
-	run_session(exp, block_1, True, exposure_conditions, all_lists_of_keyboard_constants_p, list_of_lists_allowed_digits_p, choice_nb_digits_p, instructions_partial, nb_set)
-	run_session(exp, block_2, False, exposure_conditions, all_lists_of_keyboard_constants_w, list_of_lists_char_w, choice_nb_digits_w, instructions_whole, nb_set)
+	run_session(exp, block_1, True, exposure_conditions, all_lists_of_keyboard_constants_p, all_lists_of_allowed_digits_p, choice_nb_digits_p, instructions_partial, nb_set)
+	run_session(exp, block_2, False, exposure_conditions, all_lists_of_keyboard_constants_w, all_lists_of_char_w, choice_nb_digits_w, instructions_whole, nb_set)
 
 else:
-	run_session(exp, block_2, False, exposure_conditions, all_lists_of_keyboard_constants_w, list_of_lists_char_w, choice_nb_digits_w, instructions_whole, nb_set)
-	run_session(exp, block_1, True, exposure_conditions, all_lists_of_keyboard_constants_p, list_of_lists_allowed_digits_p, choice_nb_digits_p, instructions_partial, nb_set)
+	run_session(exp, block_2, False, exposure_conditions, all_lists_of_keyboard_constants_w, all_lists_of_char_w, choice_nb_digits_w, instructions_whole, nb_set)
+	run_session(exp, block_1, True, exposure_conditions, all_lists_of_keyboard_constants_p, all_lists_of_allowed_digits_p, choice_nb_digits_p, instructions_partial, nb_set)
 	
 goodbye_message = stimuli.TextScreen("Thank you very much for participating to this experiment", f"""We wish you a nice day""")
 
